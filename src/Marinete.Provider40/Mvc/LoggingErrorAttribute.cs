d89bfa2673632bea60bb1db038db14cb8a08e964
Junior Oliveira <aoliveira@levelupgames.com.br>
2013-10-03 10:46:20 -0300

    Mvc filter error

---
+++ src/Marinete.Provider40/Mvc/LoggingErrorAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Marinete.Common.Domain;
+
+namespace Marinete.Provider40.Mvc {
+
+    public class LoggingErrorAttribute : HandleErrorAttribute {
+
+        public override void OnException(ExceptionContext context) {
+            var exception = context.Exception.InnerException ?? context.Exception;
+            var currentUser = HttpContext.Current.User.Identity.Name;
+
+            var error = new Error {
+                CurrentUser = !string.IsNullOrWhiteSpace(currentUser) ? HttpContext.Current.User.Identity.Name : "marinet",
+                Exception = exception.ToString(),
+                Message = exception.Message
+            };
+
+            var provider = new MarineteRestfulProvider();
+            provider.Error(error);
+        }
+    }
+}
