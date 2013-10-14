25c8b34798ae8b862363684e47e218bd848b77dd
Junior Oliveira <aoliveira@levelupgames.com.br>
2013-10-14 14:31:42 -0300

    Layout listing errors

--- src/Marinete.Web/app/js/filters.js
+++ src/Marinete.Web/app/js/filters.js
@@ -1,18 +1,29 @@
 'use strict';
 
 /* Filters */
+var module = angular.module('marinetFilters', []);
 
-angular.module('marinetFilters', []).filter('newlines', function () {
+module.filter('newlines', function () {
     return function (text) {
         return text.toString().replace(/\n/g, '<br/>');
     };
 });
 
-angular.module('marinetFilters', []).filter('normalizedate', function () {
-    return function (jsonDate) {
-        if (jsonDate)
-        return new Date(parseInt(jsonDate.substr(6)));
+module.filter('truncate', function () {
+    return function (text, length) {
+        var message = text;
+
+        if (message.length > length) {
+            message = $.trim(text).substring(0, length) + ' ...';
+        }
+
+        return message;
     };
 });
 
-
+module.filter('normalizedate', function () {
+    return function (jsonDate) {
+        if (jsonDate)
+            return new Date(parseInt(jsonDate.substr(6)));
+    };
+});
