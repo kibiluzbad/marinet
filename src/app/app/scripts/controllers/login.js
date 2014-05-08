6e70aa0db567964208b2571f7f21987f29ee07ce
Leonardo Cardoso <leonardo.cardoso@ctis.com.br>
2014-05-08 12:54:24 -0300

    Fix message bug

--- src/app/app/scripts/controllers/login.js
+++ src/app/app/scripts/controllers/login.js
@@ -7,6 +7,7 @@ angular.module('marinetApp')
             Auth.login($scope.user, function(){
                 $scope.$root.user = {username: $scope.user.username, role: routingConfig.userRoles.user}
                 $location.path('/apps');
+                $scope.$root.$emit('hidemessage','');
           }, function(){
                 $scope.$root.$emit('message', 'Usuário e/ou senha inválido.');
           });
--- src/app/app/scripts/controllers/message.js
+++ src/app/app/scripts/controllers/message.js
@@ -4,8 +4,11 @@ angular.module('marinetApp')
   .controller('MessageCtrl', function ($scope) {
     $scope.message = '';
     $scope.show = false;
+    $scope.$root.$on('hidemessage',function(event,args){
+        $scope.show = false;
+        $scope.message = '';
+    });
     $scope.$root.$on('message', function (event, args) {
-        console.log(args);
         $scope.show = true;
         $scope.message = args;        
     });
